ad882ddb9fab4b5e2419afb1f4e7d186e32b962c
de3p <de3pvalorant@gmail.com>
2025-08-28 08:02:03 -0500

    inital commit

---
+++ server.py
@@ -0,0 +1,58 @@
+import requests
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+
+load_dotenv()
+
+
+app = FastAPI()
+
+
+templates = Jinja2Templates(directory="templates")
+
+
+def send_message(message: str):
+    bot_token = os.getenv("BOT_TOKEN")
+    chat_id = os.getenv("BOT_CHAT")
+    
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+    
+    data = {
+        "chat_id": chat_id,
+        "text": message
+    }
+    
+    response = requests.post(url, data=data)
+    
+    print(response.content)
+    
+    if not response.ok:
+        return False
+    
+    return True
+
+
+
+class Message(BaseModel):
+    message: str
+@app.post("/message")
+async def web_message():
+    
+    send_message("it worked!")
+    
+    return {"detail": "is worked!"}
+
+
+
+@app.get("/send")
+async def client_send(request: Request):
+
+    return templates.TemplateResponse(request=request, name="index.html")
+
+
+
+if __name__ == "__main__":
+    print("hello world")
